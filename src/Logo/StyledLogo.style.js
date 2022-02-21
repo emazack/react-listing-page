3e4a8d35028b88a531e8a63c7dbd69b51f288a69
emazack <em.zaccaria@gmail.com>
2022-02-21 22:55:41 +0100

    add pointer

--- src/Logo/StyledLogo.style.js
+++ src/Logo/StyledLogo.style.js
@@ -4,4 +4,5 @@ import styled from 'styled-components';
 export const StyledLogo = styled.div`
     width: 100px;
     margin-bottom: clamp(20px, calc(1.25rem + ((1vw - 3.75px) * 1.9417)), 50px);
+    cursor: pointer;
 `;
